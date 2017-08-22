f3c04ced25ea6158bb6f9a91cb46169b00dc502c
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-22 09:30:09 -0400

    Add session controller, nested routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :signed_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+
   attr_reader :password
+  
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
