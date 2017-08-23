67e9e82bda92460666a5a2f585e4d9cb8fc8cd04
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-23 09:41:27 -0400

    Fix migration issue

--- db/migrate/20170822122454_create_users.rb
+++ db/migrate/20170822122454_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :username, null: false
       t.string :session_token, null: false
       t.string :password_digest, null: false
-      t.string :email, null: false
       t.timestamps
     end
   end
