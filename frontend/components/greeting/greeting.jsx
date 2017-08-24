9c8bcb01e441cf862e135716f647e818e5b290f7
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-24 14:29:33 -0400

    Add and style sidebar buttons

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -10,12 +10,28 @@ const sessionLinks = () => (
 );
 
 const personalize = (currentUser, logout) => (
+  <div>
   <section className="auth-container">
     <div className="header sidebar-branding">
       <span className="logo"></span>
     </div>
-    <button className="logout header-button" onClick={logout.bind(this)}>Logout</button>
+
+    <div className="sidebar">
+      <div className="side-section section-a">
+          <i className="fa fa-plus" aria-hidden="true"></i>
+          <i className="fa fa-calendar-check-o" aria-hidden="true"></i>
+          <i className="fa fa-search" aria-hidden="true"></i>
+      </div>
+
+      <div className="side-section section-b">
+        <i className="fa fa-file-text-o" aria-hidden="true"></i>
+        <i className="fa fa-book" aria-hidden="true"></i>
+        <i className="fa fa-tag" aria-hidden="true"></i>
+      </div>
+    </div>
   </section>
+  <button className="logout header-button" onClick={logout}>Logout</button>
+ </div>
 );
 
 
