5409b2da77739917ca943d16c63307c5e07f2fb6
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-24 15:44:33 -0400

    Add header to note index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Note.destroy_all
+
+
+15.times do
+
+Note.create!(title: 'Lorem Ipsum', body: 'Master cleanse pickled franzen trust fund woke fashion axe. Affogato skateboard everyday carry fashion axe DIY, letterpress bespoke heirloom VHS try-hard gentrify. Paleo hoodie tote bag whatever. Williamsburg fashion axe drinking vinegar meh iceland. Tousled af quinoa DIY lumbersexual cred flannel mlkshk bespoke 3 wolf moon', author_id: 1, notebook_id: 1, archived: false);
+
+end
