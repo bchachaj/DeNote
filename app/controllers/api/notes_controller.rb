3c56643e439344d599aea56a70cf446f95341b69
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-24 09:51:37 -0400

    add note controller

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,23 +1,46 @@
 class Api::NotesController < ApplicationController
-  def new
+
+  def index
+    @notes = Note.all
   end
 
   def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      render :show
+    else
+      render json: @note.errors.full_messages, status: 422
+    end
+
   end
 
   def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      render :show
+    else
+      render json: @note.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @note = Note.find(params[:id])
+
+    if @note.destroy
+      render :show
+    else
+      render json: @note.errors.full_messages, status: 422
+    end
+
   end
 
-  def index
-  end
 
   private
 
   def note_params
-    params.permit(:note).require(:title, :body)
+    params.require(:note).permit(:title, :body)
   end
 
 
