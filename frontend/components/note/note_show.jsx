ba242ad56f4f0d339410cfbace1b4d2df82271d9
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-25 14:57:24 -0400

    Edit note functionality added

--- frontend/components/note/note_show.jsx
+++ frontend/components/note/note_show.jsx
@@ -9,19 +9,35 @@ class NoteShow extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      id: '',
       title: '',
       body: ''
     };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  update(property) {
+    return e => this.setState({ [property]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.state.id = this.props.note.id;
+    this.props.requestUpdateNote(this.state);
   }
 
   componentDidMount(){
     this.props.requestSingleNote(this.props.match.params.noteId);
-    debugger;
   }
 
   componentWillReceiveProps(nextProps) {
-
+    if(this.props.note === undefined && nextProps.note) {
+      this.setState({
+         title: nextProps.note.title,
+         body: nextProps.note.body
+       });
+    }
     if(this.props.match.params.noteId !== nextProps.match.params.noteId) {
       this.props.requestSingleNote(nextProps.match.params.noteId);
     }
@@ -35,12 +51,23 @@ class NoteShow extends React.Component {
     return (
       <div className="note-show">
         <h1>{note.title}</h1>
-        <form>
-            <input type="text" className="note-header-input" value={note.title}/>
-            <input type="textarea" rows="40" cols="50" className="note-header-input" value={note.body}/>
-            <input type="submit" value="Save" onSubmit={this.handleSubmit}/>
+        <form onSubmit={this.handleSubmit}>
+            <input type="text"
+                   className="note-title-input"
+                   onChange={this.update('title')}
+                   value={this.state.title}/>
+
+            <input type="textarea"
+                   rows="40" cols="50"
+                   className="note-body-input"
+                   onChange={this.update('body')}
+                   value={this.state.body}/>
+
+            <input className="temp-note-save" type="submit"
+                   value="Save"
+                   />
+
 .        </form>
-        <p>{note.body}</p>
       </div>
     );
   }
