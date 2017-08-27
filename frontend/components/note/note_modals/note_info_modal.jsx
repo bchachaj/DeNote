920a2ed4bb5a13d4b3f0d1f9ae48cbb950b21f80
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-27 19:04:58 -0400

    Constructing note modals

---
+++ frontend/components/note/note_modals/note_info_modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+class NoteInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Note Information</h1>
+      </div>
+    );
+  }
+}
+
+export default NoteInfo;
