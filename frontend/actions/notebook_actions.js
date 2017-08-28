4dcb459355d4707a3c49bf267374e14ce0d17d32
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-28 16:54:42 -0400

    create notebook actions

---
+++ frontend/actions/notebook_actions.js
@@ -0,0 +1,57 @@
+import * as NotebookAPI from './util/notebook_util.js';
+
+
+export const RECEIVE_ALL_NOTEBOOKS = 'RECEIVE_ALL_NOTEBOOKBOOKS';
+export const RECEIVE_ONE_NOTEBOOK = 'RECEIVE_ONE_NOTEBOOK';
+
+export const REMOVE_NOTEBOOK = 'REMOVE_NOTEBOOK';
+
+export const CREATE_NOTEBOOK = 'CREATE_NOTEBOOK';
+
+
+///INDEX
+export const requestAllNotebooks = () => (dispatch) => {
+  return NotebookAPI.fetchAllNotes()
+    .then(nb => dispatch(receiveAllNotebooks(nb))
+  );
+};
+
+
+export const receiveAllNotebooks = (notebooks) => ({
+  type: RECEIVE_ALL_NOTEBOOKS,
+  notebooks,
+});
+
+
+//SHOW
+export const requestSingleNotebook = (id) => dispatch => {
+  return NotebookAPI.fetchOneNotebook(id)
+    .then(notebook => dispatch(receiveOneNotebook(notebook)));
+};
+
+export const receiveOneNotebook = (note) => ({
+  type: RECEIVE_ONE_NOTEBOOK,
+  note
+});
+
+//CREATE
+
+export const createNotebook = notebook => dispatch => {
+  return NotebookAPI.createNotebook(notebook)
+    .then(cnotebook => {dispatch(receiveOneNotebook(cnotebook));
+      return cnotebook;
+    }
+  );
+};
+
+//DELETE
+
+export const deleteNotebook = notebook => dispatch => {
+  return NotebookAPI.deleteNotebook(notebook)
+    .then(dnotebook => dispatch(removeNotebook(dnotebook)));
+};
+
+export const removeNotebook = notebook => ({
+  type: REMOVE_NOTEBOOK,
+  notebook
+});
