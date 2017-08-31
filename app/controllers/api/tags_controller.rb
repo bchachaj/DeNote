65b4b26cfc6f4ea26ef03bec0bcaf898a23e4e65
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-08-31 19:27:59 -0400

    Add tag controller

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,36 @@
+class Api::TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def create
+    @tag = Tag.new(t_params)
+    @tag.author_id = current_user.id
+
+    if @tag.save
+      render :show
+    else
+      render json: @tag.errors.full_messages, status: 422
+    end
+
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    if @tag.delete
+      render :show
+    else
+      render json: @tag.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  private
+
+  def t_params
+    params.require(:tag).permit(:name, :author_id)
+  end
+end
